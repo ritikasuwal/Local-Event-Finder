71276b6ceb6cd9453914c7aca66e24b273ed10a0
Rabin Pyakurel <rabinstar137@gmail.com>
2025-12-09 03:56:06 +0545

    init: project backend

---
+++ backend/src/main/java/com/rabin/backend/Model/User.java
@@ -0,0 +1,50 @@
+package com.rabin.backend.Model;
+
+import com.rabin.backend.Enums.UserStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name= "event_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ backend/src/main/java/com/rabin/backend/enums/UserStatus.java
@@ -0,0 +1,7 @@
+package com.rabin.backend.Enums;
+
+public enum UserStatus {
+    ACTIVE,
+    INACTIVE,
+    BLOCKED
+}
